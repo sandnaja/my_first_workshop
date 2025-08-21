84649a37616fc6bf7981f21f0a59fff87b0b7c19
Umant <umnat@sokochan.com>
2025-08-21 09:52:56 +0700

    feat: update LandmarkCard to display description and price; enhance type definition for LandmarkCardProps

--- components/card/LandmarkCard.tsx
+++ components/card/LandmarkCard.tsx
@@ -3,7 +3,7 @@ import { LandmarkCardProps } from "@/utils/types";
 import LandmarkRating from "./LandmarkRating";
 
 const LandmarkCard = ({landmark}:{landmark:LandmarkCardProps}) => {
-  const { name, image ,id ,province , lat ,lng ,category} = landmark;
+  const { name, image ,id ,description ,price ,province , lat ,lng ,category} = landmark;
   return (
     <article className="group relative">
       <div className="relative h-[300px] rounded-md mb-2">
@@ -15,9 +15,19 @@ const LandmarkCard = ({landmark}:{landmark:LandmarkCardProps}) => {
           className="object-cover rounded-md group-hover:scale-105 transition-transform duration-500"
         />
       </div>
+
       <div className="flex items-center justify-between">
-        <h3 className="text-sm font-semibold mt-1">{name}</h3>
+        <h3 className="text-sm font-semibold mt-1">{name.substring(0, 30)}</h3>
         <LandmarkRating/>
+      </div>
+
+      <p className="text-sm mt-1 text-muted-foreground">
+        {description.substring(0, 40)}
+      </p>
+
+      <div className="mt-1 flex items-center justify-between font-semibold text-sm">
+        <span>{price} ฿</span>
+        <p>{province}</p>
       </div>
     </article>
   )
